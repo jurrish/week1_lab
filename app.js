c3c8ffe06e9e03fa8333ab2a10286ee9f3635007
Frank Iriarte <jurrish@gmail.com>
2016-04-09 15:11:51 -0700

    finished. just working on stretch question

--- app.js
+++ app.js
@@ -4,40 +4,44 @@ var userName = prompt(question);
 alert('Hello ' + userName + '! Welcome to my Page!');
 console.log(userName);
 
-var guessFavMovie = prompt('Is my favorite movie Shawshank Redemption? Y/N');
-guessFavMovie = guessFavMovie.toUpperCase();
-if(guessFavMovie === 'Y') {
-  alert('That is correct!');
-  counter++;
-  console.log(userName + ' answered : ' + guessFavMovie);
-} else {
-  alert('Incorrect. My favorite movie IS Shawshank Redemption');
-  console.log(userName + ' answered ' + guessFavMovie);
-}
+// var guessFavMovie = prompt('Is my favorite movie Shawshank Redemption? Y/N');
+// guessFavMovie = guessFavMovie.toUpperCase();
+// if(guessFavMovie === 'Y' || guessFavMovie === 'YES') {
+//   alert('That is correct!');
+//   counter++;
+//   console.log(userName + ' answered : ' + guessFavMovie);
+// } else {
+//   alert('Incorrect. My favorite movie IS Shawshank Redemption');
+//   console.log(userName + ' answered ' + guessFavMovie);
+// }
+//
+// var guessFavFood = prompt('Is my favorite food pizza? Y/N');
+// guessFavFood = guessFavFood.toUpperCase();
+// if(guessFavFood === 'N' || guessFavFood === 'NO') {
+//   alert('That\'s correct. My favorite food is sushi, not pizza.');
+//   console.log(userName + ' answered ' + guessFavFood);
+//   counter++;
+// } else if(guessFavFood === '') {
+//   alert('You didn\'t enter a value');
+// } else {
+//   alert('You are incorrect. Pizza is not my favorite food. Sushi is.');
+//   console.log(userName + ' answered ' + guessFavFood);
+// }
+//
+// var guessFavColor = prompt('Is my favorite color blue? Y/N');
+// guessFavColor = guessFavColor.toUpperCase();
+// if(guessFavColor === 'Y' || guessFavColor === 'YES') {
+//   alert('That\'s correct!');
+//   console.log(userName + ' answered ' + guessFavColor);
+//   counter++;
+// } else if(guessFavColor === '') {
+//   alert('You didn\t enter a value');
+// } else {
+//   alert('Incorrect. My favorite color is blue');
+//   console.log(userName + ' answered ' + guessFavColor);
+// }
 
-var guessFavFood = prompt('Is my favorite food pizza? Y/N');
-guessFavFood = guessFavFood.toUpperCase();
-if(guessFavFood !== 'Y') {
-  alert('That\'s correct. My favorite food is sushi, not pizza.');
-  console.log(userName + ' answered ' + guessFavFood);
-  counter++;
-} else {
-  alert('You are incorrect. Pizza is not my favorite food. Sushi is.');
-  console.log(userName + ' answered ' + guessFavFood);
-}
-
-var guessFavColor = prompt('Is my favorite color blue? Y/N');
-guessFavColor = guessFavColor.toUpperCase();
-if(guessFavColor === 'Y') {
-  alert('That\'s correct!');
-  console.log(userName + ' answered ' + guessFavColor);
-  counter++;
-} else {
-  alert('Incorrect. My favorite color is blue');
-  console.log(userName + ' answered ' + guessFavColor);
-}
-
-var guessMyNumber = prompt('What do you think my favorite number is? Guess 1-10');
+var guessMyNumber = parseInt(prompt('What do you think my favorite number is? Guess 1-10'));
 if(guessMyNumber < 7) {
   alert('Sorry ' + userName + ' your guess is too low');
   console.log(userName + ' answered ' + guessMyNumber);
@@ -45,9 +49,9 @@ if(guessMyNumber < 7) {
   alert('Sorry ' + userName + ' your guess is too high');
   console.log(userName + ' answered ' + guessMyNumber);
 } else {
-  alert('Great guess! My favorite number is 7');
+  alert('Good guess!');
   console.log(userName + ' answered ' + guessMyNumber);
   counter++;
 }
-
-var correctGuesses = alert('Congrats, ' + userName + ' you got ' + counter + ' out of 4 questions correct!');
+//
+// var correctGuesses = alert('Congrats, ' + userName + ' you got ' + counter + ' out of 4 questions correct!');
