c6a320c9a065a885832f6b56615062eefc39d09e
Frank Iriarte <jurrish@gmail.com>
2016-04-09 13:36:22 -0700

    finished 2nd/3rd problem concerning toUpperCase error checking, moving on to empty string entries for all 3 questions

--- app.js
+++ app.js
@@ -3,34 +3,36 @@ var userName = prompt(question);
 alert('Hello ' + userName + '! Welcome to my Page!');
 console.log(userName);
 
-var guessFavMovie = prompt('Is my favorite movie Shawshank Redemption? Y/N');
-guessFavMovie = guessFavMovie.toUpperCase();
-if(guessFavMovie === 'Y' || 'YES') {
-  alert('That is correct!');
-  console.log(userName + ' answered ' + guessFavMovie);
-} else {
-  alert('Incorrect! Shawshank Redemption is my favorite movie');
-  console.log(userName + ' answered ' + guessFavMovie);
-}
+// var guessFavMovie = prompt('Is my favorite movie Shawshank Redemption? Y/N');
+// guessFavMovie = guessFavMovie.toUpperCase();
+// if(guessFavMovie === 'Y' || 'YES') {
+//   alert('That is correct!');
+//   console.log(userName + ' answered ' + guessFavMovie);
+// } else {
+//   alert('Incorrect! Shawshank Redemption is my favorite movie');
+//   console.log(userName + ' answered ' + guessFavMovie);
+// }
 
 // var guessFavFood = prompt('Is my favorite food pizza? Y/N');
-// if(guessFavFood !== 'Y'){
+// guessFavFood = guessFavFood.toUpperCase();
+// if(guessFavFood !== 'Y' || 'YES') {
 //   alert('That\'s correct. My favorite food is sushi, not pizza.');
 //   console.log(userName + ' answered ' + guessFavFood);
 // } else {
 //   alert('You are incorrect. Pizza is not my favorite food. Sushi is.');
 //   console.log(userName + ' answered ' + guessFavFood);
 // }
-//
+
 // var guessFavColor = prompt('Is my favorite color blue? Y/N');
-// if(guessFavColor === 'Y') {
+// guessFavColor = guessFavColor.toUpperCase();
+// if(guessFavColor === 'Y' || 'YES') {
 //   alert('That\'s correct!');
 //   console.log(userName + ' answered ' + guessFavColor);
 // } else {
 //   alert('Incorrect. My favorite color is blue');
 //   console.log(userName + ' answered ' + guessFavColor);
 // }
-//
+
 // var guessMyNumber = prompt('What do you think my favorite number is? Guess 1-10');
 // if(guessMyNumber < 7) {
 //   alert('Sorry ' + userName + ' your guess is too low');
